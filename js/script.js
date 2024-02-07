8923a26c8fb03050cc1c13cb77c6f41645296402
MrBorker <anastasiakondratyushechka@gmail.com>
2024-02-07 14:48:21 +0100

    final

--- js/script.js
+++ js/script.js
@@ -35,16 +35,63 @@ const taskDoneBtn = ".task__button-done";
 
 // Functions
 
+const getUniqueId = function () {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+};
+
 const removeChildNodes = function (parent) {
   while (parent.firstChild) {
     parent.firstChild.remove();
   }
 };
 
-const getUniqueId = function () {
-  return Math.floor((1 + Math.random()) * 0x10000)
-    .toString(16)
-    .substring(1);
+const renderTasks = function (array) {
+  removeChildNodes(tasksContainer);
+  array.forEach((task) => {
+    const taskGenerator = taskTemplate.content.cloneNode(true);
+    const taskWrapper = taskGenerator.querySelector(".task__wrapper");
+    const taskChecker = taskGenerator.querySelector(".task__checker");
+    const taskTitle = taskGenerator.querySelector(".task__title");
+    const editBtn = taskGenerator.querySelector(".task__button-edit");
+    const removeBtn = taskGenerator.querySelector(".task__button-remove");
+    const removeDoneBtn = taskGenerator.querySelector(
+      ".task__button-remove-done"
+    );
+    taskWrapper.dataset.taskNumber = task.id;
+    taskTitle.textContent = task.text;
+    if (task.status === true) {
+      taskChecker.classList.remove("task__checker-done");
+      taskTitle.classList.remove("task__title-done");
+      editBtn.classList.remove("hidden");
+      removeBtn.classList.remove("hidden");
+      removeDoneBtn.classList.add("hidden");
+    } else {
+      taskChecker.classList.add("task__checker-done");
+      taskTitle.classList.add("task__title-done");
+      editBtn.classList.add("hidden");
+      removeBtn.classList.add("hidden");
+      removeDoneBtn.classList.remove("hidden");
+    }
+    tasksContainer.append(taskWrapper);
+    const input = document
+      .querySelector(`[data-task-number='${task.id}']`)
+      .querySelector(".task__input-edit");
+    input.value = `${task.text}`;
+  });
+  taskButtons(taskCheckerBtn, handleCrossOffTaskBtnClick);
+  taskButtons(taskRemoverBtn, handleDeleteTaskBtnClick);
+  taskButtons(taskRemoverDoneBtn, handleDeleteTaskBtnClick);
+  taskButtons(taskEditorBtn, handleEditTaskBtnClick);
+  taskButtons(taskDoneBtn, handleDoneBtnClick);
+  renewStatistics(array);
+};
+
+const renderAndSave = function (array) {
+  renderTasks(array);
+  localStorage.tasks = JSON.stringify(array);
+  defaultFilter.checked = true;
 };
 
 const getNodes = function () {
@@ -56,12 +103,6 @@ const getNodes = function () {
   const doneBtn = taskWrapper.querySelector(".task__button-done");
   const title = taskWrapper.querySelector(".task__title");
   return { taskWrapper, index, input, taskCheckerBtn, doneBtn, title };
-};
-
-const renderAndSave = function (array) {
-  renderTasks(array);
-  localStorage.tasks = JSON.stringify(array);
-  defaultFilter.checked = true;
 };
 
 const taskButtons = function (selector, action) {
@@ -111,47 +152,6 @@ const renewStatistics = function (array) {
   doneTaskQuantity.textContent = array.filter(
     (task) => task.status === false
   ).length;
-};
-
-const renderTasks = function (array) {
-  removeChildNodes(tasksContainer);
-  array.forEach((task) => {
-    const taskGenerator = taskTemplate.content.cloneNode(true);
-    const taskWrapper = taskGenerator.querySelector(".task__wrapper");
-    const taskChecker = taskGenerator.querySelector(".task__checker");
-    const taskTitle = taskGenerator.querySelector(".task__title");
-    const editBtn = taskGenerator.querySelector(".task__button-edit");
-    const removeBtn = taskGenerator.querySelector(".task__button-remove");
-    const removeDoneBtn = taskGenerator.querySelector(
-      ".task__button-remove-done"
-    );
-    taskWrapper.dataset.taskNumber = task.id;
-    taskTitle.textContent = task.text;
-    if (task.status === true) {
-      taskChecker.classList.remove("task__checker-done");
-      taskTitle.classList.remove("task__title-done");
-      editBtn.classList.remove("hidden");
-      removeBtn.classList.remove("hidden");
-      removeDoneBtn.classList.add("hidden");
-    } else {
-      taskChecker.classList.add("task__checker-done");
-      taskTitle.classList.add("task__title-done");
-      editBtn.classList.add("hidden");
-      removeBtn.classList.add("hidden");
-      removeDoneBtn.classList.remove("hidden");
-    }
-    tasksContainer.append(taskWrapper);
-    const input = document
-      .querySelector(`[data-task-number='${task.id}']`)
-      .querySelector(".task__input-edit");
-    input.value = `${task.text}`;
-  });
-  taskButtons(taskCheckerBtn, handleCrossOffTaskBtnClick);
-  taskButtons(taskRemoverBtn, handleDeleteTaskBtnClick);
-  taskButtons(taskRemoverDoneBtn, handleDeleteTaskBtnClick);
-  taskButtons(taskEditorBtn, handleEditTaskBtnClick);
-  taskButtons(taskDoneBtn, handleDoneBtnClick);
-  renewStatistics(array);
 };
 
 // Buttons
